5ee2441e004daf6602bfca0f5823c07ea3a10ba2
Pswilliamh <pswilliamh@gmail.com>
2026-02-07 12:05:54 +0700

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import LandingPage from './pages/LandingPage';
+
+/**
+ * Main application component
+ * Rebuild trigger: 2026-02-05
+ */
+function App() {
+  return <LandingPage />;
+}
+
+export default App;
